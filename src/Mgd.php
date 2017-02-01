cb052fad504f8bca603f0db59b83e3132e81847f
Rémi Darricau <remi@copromatic.com>
2017-02-01 19:22:03 +0100

    Post Put fonctionnel

--- src/Mgd.php
+++ src/Mgd.php
@@ -66,14 +66,14 @@ class Mgd {
     }
 
     public function post($url, $object, $entityClass) {
-        $response = $this->client->fetch($this->apiUrl . $url . '.json',$this->serializer->serialize($object),\OAuth2\Client::HTTP_METHOD_POST);
+        $response = $this->client->fetch($this->apiUrl . $url . '.json',$this->serializer->serialize($object),\OAuth2\Client::HTTP_METHOD_POST,array('Content-Type' => 'application/x-www-form-urlencoded'),\OAuth2\Client::HTTP_FORM_CONTENT_TYPE_APPLICATION);
         if(self::getError($response))
             return self::post($url, $object, $entityClass);
         return $this->parser->parse($response['result'],$entityClass);
     }
 
     public function put($url, $id, $object, $entityClass) {
-        $response = $this->client->fetch($this->apiUrl . $url .'/'.$id. '.json',$this->serializer->serialize($object),\OAuth2\Client::HTTP_METHOD_PUT,array('Content-Type' => 'x-www-form-urlencoded'),\OAuth2\Client::HTTP_FORM_CONTENT_TYPE_APPLICATION);
+        $response = $this->client->fetch($this->apiUrl . $url .'/'.$id. '.json',$this->serializer->serialize($object),\OAuth2\Client::HTTP_METHOD_PUT,array('Content-Type' => 'application/x-www-form-urlencoded'),\OAuth2\Client::HTTP_FORM_CONTENT_TYPE_APPLICATION);
         if(self::getError($response))
             return self::put($url, $id, $object, $entityClass);
         return $this->parser->parse($response['result'],$entityClass);
