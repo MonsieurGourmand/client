964e637ed8f453c051a66b32e175025c6d46e0d4
TomYoYo <tom.pueyo@gmail.com>
2017-10-19 16:05:29 +0200

    supplier users

--- src/Route/Supplier/User.php
+++ src/Route/Supplier/User.php
@@ -12,38 +12,31 @@ use Mgd\Mgd;
 
 class User
 {
-    public function __construct(\Mgd\Entity\Firm $firm)
+    public function __construct(\Mgd\Entity\Supplier $supplier)
     {
-        $this->master = $firm->getMaster();
+        $this->master = $supplier->getMaster();
         $this->entity = \Mgd\Entity\User::class;
-        $this->url = Mgd::GROUPS_ROAD.$this->master->idFirm."/firms/".$firm->getIdFirm()."/users";
+        $this->url = "/suppliers/" . $supplier->getIdSupplier() . "/users";
     }
 
-    public function getAll($format=\Mgd\Mgd::FORMAT_OBJECT)
+    public function getAll($format = \Mgd\Mgd::FORMAT_OBJECT)
     {
         $params = array();
-        return $this->master->getAll($this->url, $this->entity,$params,$format);
+        return $this->master->getAll($this->url, $this->entity, $params, $format);
     }
 
-    public function get($id,$format=\Mgd\Mgd::FORMAT_OBJECT)
+    public function get($id, $format = \Mgd\Mgd::FORMAT_OBJECT)
     {
-        return $this->master->get($this->url,$id,$this->entity,$format);
+        return $this->master->get($this->url, $id, $this->entity, $format);
     }
 
-    public function post(\Mgd\Entity\User $user,$format=\Mgd\Mgd::FORMAT_OBJECT)
+    public function post(\Mgd\Entity\User $user, $format = \Mgd\Mgd::FORMAT_OBJECT)
     {
-        return $this->master->post($this->url,$user,$this->entity,$format);
+        return $this->master->post($this->url, $user, $this->entity, $format);
     }
 
-    public function put(\Mgd\Entity\User $user,$format=\Mgd\Mgd::FORMAT_OBJECT)
+    public function put(\Mgd\Entity\User $user, $format = \Mgd\Mgd::FORMAT_OBJECT)
     {
-        return $this->master->put($this->url,$user->getId(),$user,$this->entity,$format);
+        return $this->master->put($this->url, $user->getId(), $user, $this->entity, $format);
     }
-
-    public function remove(\Mgd\Entity\User $user)
-    {
-        return $this->master->remove($this->url,$user->getId());
-    }
-
-
 }
